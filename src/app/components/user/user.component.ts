8bee3664403dc5117a412a6127a1680a1d65094c
Tetiankaa <tanyakorsun0208@gmail.com>
2023-12-05 13:53:41 +0100

    lesson 1

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,18 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IUser} from "../../interfaces";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input() user:IUser
+  @Output() lift = new EventEmitter<IUser>()
+
+  getDetails():void{
+    this.lift.emit(this.user)
+  }
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {IUser} from "../../interfaces";
+import {UserComponent} from "../user/user.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {UserDetailsComponent} from "../user-details/user-details.component";
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [
+    UserComponent,
+    NgForOf,
+    UserDetailsComponent,
+    NgIf
+  ],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent implements OnInit{
+    users:IUser[];
+    userDetails:IUser;
+
+    constructor(private userService:UserService) {
+    }
+
+  ngOnInit(): void {
+      this.userService.getAll().subscribe(value => this.users = value)
+  }
+
+  getUser(userDetail: IUser) {
+    this.userDetails = userDetail;
+  }
+}
