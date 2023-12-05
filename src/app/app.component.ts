f2b3dcd8838f8fdda2fa4902ad27ad5dea1e7bc9
Tetiankaa <tanyakorsun0208@gmail.com>
2023-12-05 14:04:10 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {UsersComponent} from "./components/users/users.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UsersComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
