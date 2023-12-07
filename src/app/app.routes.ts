0d1d3a7d2a1a6fdcfe69cbdeaa4b8f4b4358c18b
Tetiankaa <tanyakorsun0208@gmail.com>
2023-12-07 12:09:42 +0100

    lesson2 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {PostsPageComponent, UserDetailsPageComponent, UsersPageComponent} from "./pages";
+import {MainLayoutComponent} from "./layouts";
+
+
+export const routes: Routes = [
+  {path:'',component:MainLayoutComponent, children:[
+      {path:'',redirectTo:'users',pathMatch:"full"},
+      {path:'users',component:UsersPageComponent,children:[
+          {path:':id', component:UserDetailsPageComponent}
+        ]},
+      {path:'posts',component:PostsPageComponent}
+    ]}
+];
---
+++ src/app/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './posts-page/posts-page.component';
+export * from './users-page/users-page.component';
+export * from './user-details-page/user-details-page.component';
